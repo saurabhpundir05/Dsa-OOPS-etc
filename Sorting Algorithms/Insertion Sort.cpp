36e16b1060cd66f76206bd137b211b3a0f8ba3b3
Saurabh Pundir <75524401+saurabhpundir05@users.noreply.github.com>
2025-07-05 13:01:29 +0530

    Update Insertion Sort.cpp

--- Sorting Algorithms/Insertion Sort.cpp
+++ Sorting Algorithms/Insertion Sort.cpp
@@ -1,1 +1,34 @@
-
+//Insertion Sort
+#include<iostream>
+using namespace std;
+void insertionSort(int a[],int n){
+    for(int i=0;i<n;i++){
+        int c=a[i];
+        int j=i-1;
+        while(a[j]>c && j>=0){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=c;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter Range"<<endl;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<"Before Sort"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    insertionSort(a,n);
+    cout<<"After Sort"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
